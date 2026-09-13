4716a850f3d4bce06cefa835f0c7dbd1fcb636b2
edenagboden odiase <engrodiase1966@gmail.com>
2026-09-13 20:23:38 +0100

    update all orders page to react

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom"
 import {HomePage} from "./pages/HomePage"
 import {CheckoutPage} from "./pages/CheckoutPage"
+import {OrdersPage} from "./pages/OrdersPage"
 import './App.css'
 
 function App() {
@@ -8,7 +9,9 @@ function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
+     
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
     </Routes>
   )
 }
