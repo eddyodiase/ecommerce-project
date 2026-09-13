dd080ffd5e4b23f1449b51412010427f6b87d442
edenagboden odiase <engrodiase1966@gmail.com>
2026-09-13 17:20:07 +0100

    update changes

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -7,8 +7,8 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
     </Routes>
   )
 }
